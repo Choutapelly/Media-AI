55b74000df5c3928a2f067857638e03247d36513
Choutapelly <choutapelly@hotmail.com>
2025-03-11 01:22:41 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+``javascript
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AIMediaEditor from './AIMediaEditor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AIMediaEditor />
+  </React.StrictMode>
+);
+```
